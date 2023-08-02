1f42c4f5c2153f42e300eef87b379c7f01edd3e7
Brent Vatne <brentvatne@gmail.com>
2023-08-02 15:15:42 -0700

    Fix a few syncing behaviors - initial sync, do not send when disabled, and send when re-enabled

--- apps/server/server.js
+++ apps/server/server.js
@@ -12,11 +12,23 @@ app.use(cors());
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
+let previousMessage = null;
+
 wss.on("connection", function connection(ws) {
+  // Send previous message to new connections to seed initial state
+  if (previousMessage) {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(previousMessage);
+      }
+    });
+  }
+
   ws.on("message", function incoming(message) {
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message.toString());
+        previousMessage = message.toString();
       }
     });
   });
