cf2816973c0c5cb7a7985d777f7e50028117a602
Brent Vatne <brentvatne@gmail.com>
2023-08-03 15:21:41 -0700

    Send previous message on init instead

--- apps/server/index.js
+++ apps/server/index.js
@@ -2,15 +2,13 @@ let previousMessage = null;
 
 export default {
   onConnect(websocket, room) {
-    if (previousMessage) {
-      websocket.send(previousMessage);
-    }
-
     websocket.addEventListener("message", (event) => {
       const message = event.data;
 
       if (event.data === 'init') {
-        websocket.send(previousMessage);
+        if (previousMessage) {
+          websocket.send(previousMessage);
+        }
       } else {
         room.broadcast(message.toString());
         previousMessage = message.toString();
