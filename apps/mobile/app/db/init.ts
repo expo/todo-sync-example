c4157e156377c7c8830c3dd9eec4f3ba7d9fe6a2
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-09 21:42:06 -0400

    create a db provider for use by the sync layer

--- apps/mobile/app/db/init.ts
+++ apps/mobile/app/db/init.ts
@@ -1,20 +1,27 @@
 import * as SQLite from "expo-sqlite";
+import { createSingletonDbProvider } from "../sync/SyncedExpoDB";
+import { cryb64 } from "@vlcn.io/ws-common";
 
 export const db = SQLite.openDatabase("test.db");
 
+// TODO: ideally we can share this in a package between client and server
+// Also see -- https://github.com/vlcn-io/cr-sqlite/issues/318
+const schema = [
+  `CREATE TABLE IF NOT EXISTS "todo" ("id" PRIMARY KEY, "text", "completed" INTEGER DEFAULT 0);`,
+  `SELECT crsql_as_crr('todo');`
+];
+
 export function initDatabase() {
   db.exec(
-    [
-      {
-        sql: 'CREATE TABLE IF NOT EXISTS "todo" ("id" PRIMARY KEY, "text", "completed" INTEGER DEFAULT 0);',
-        args: [],
-      },
-      {
-        sql: "SELECT crsql_as_crr('todo');",
-        args: [],
-      },
-    ],
+    schema.map((sql) => ({ sql, args: [] })),
     false,
     () => {}
   );
 }
+
+export const dbProvider = createSingletonDbProvider({
+  dbName: "test.db",
+  db,
+  schemaName: "test",
+  schemaVersion: cryb64(schema.join("\n")),
+});
